6ca7b753a99ec2370b9f9dac421f91c6a5a615ac
ebubeosayi-create <ebubeosayi@gmail.com>
2026-07-31 20:48:08 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your Age"))
+if age > 14:
+    print("You can ride a big bicycle!")
+else:
+    print("You can only ridea small bicycle")
+
+grade=int(input("Enter your Grade"))
+if grade > 8:
+    print("You can learn physics")
+else:
+    print("you can only learn science")
